000594d92020c8685bdfec37dfe67310d15b9477
Ryan Nichols <ryannichols@MacBook-Air-7.local>
2026-03-10 13:18:32 +0000

    remove programming project extra folder

--- test.java
+++ test.java
@@ -1,5 +1,12 @@
-public class test {
-    public static void main(String[] args) {
+public class Test {
+public static void count(int x){
+for(int i = 0; i < x; i++){
+    System.out.println(i);
+}
+}
+public static void main(String[] args) {
         System.out.println("hi team");
+        count(5);
     }
+
 }
